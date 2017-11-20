66a23db09ece5ef40ac5e436e9e780ab54ad8e5d
LuFoo <lucyk2406@gmail.com>
2017-11-21 00:05:51 +0100

    A2-Devise: chose relevant devise modules

--- db/migrate/20171120225626_add_devise_to_users.rb
+++ db/migrate/20171120225626_add_devise_to_users.rb
@@ -2,12 +2,14 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
+      t.string :name
+      t.string :first_name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+        # t.string   :reset_password_token
+        # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -36,7 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
