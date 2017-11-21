a3a2be2ed1d80f80ecff83780833931d3a9dca34
LuFoo <lucyk2406@gmail.com>
2017-11-21 01:08:34 +0100

    A2-Devise: changed application nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :registerable, :rememberable, :trackable
+  # :recoverable, :rememberable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 end
