f886888be2f0a679b344381af621c048dd9926bd
LuFoo <lucyk2406@gmail.com>
2017-11-22 17:40:01 +0100

    A2-Rspec: created feature test for s001 and s003

---
+++ spec/features/s001_users_see_offers_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+describe 'S001 See all offers' do
+  context '(when logged in)' do
+    before :each do
+      user = User.create(email: 'test@example.de', password: 'psw', password_confirmation: 'psw')
+      login_as(user, :scope => :user)
+    end
+    it 'can see all offers' do
+        visit offers_path
+        end
+    end
+end
--- spec/features/s003_users_see_vendors_spec.rb
+++ spec/features/s003_users_see_vendors_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
-describe 'S003 See all offers' do
+describe 'S003 See all vendors' do
   context '(when logged in)' do
     before :each do
       user = User.create(email: 'test@example.de', password: 'psw', password_confirmation: 'psw')
       login_as(user, :scope => :user)
     end
-    it 'can see all offers' do
-        visit offers_path
+    it 'can see all vendors' do
+        visit vendors_path
         end
     end
 end
