d1ef72c20f7d35f1544741feafa21adc2800210c
LuFoo <lucyk2406@gmail.com>
2017-12-08 16:10:25 +0100

    S001-S005: fixed Rspec Testing user login

--- spec/features/s002_vendor_can_create_offer_spec.rb
+++ spec/features/s002_vendor_can_create_offer_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 describe 'S002 vendor can create offers' do
     before :each do
-      user = User.create(name: 'Tester', email: 'test@example.de', password: 'psw', password_confirmation: 'psw')
+      user = User.create!(name: 'Tester', email: 'test@example.de', password: 'psw123', password_confirmation: 'psw123')
       login_as(user, :scope => :user)
+      category = Category.create(name: 'Books')
     end
     it 'by creating an offer' do
         visit new_offer_path
-
         fill_in 'offers_title', with: 'Hochzeitskleid'
-        fill_in 'offers_category_id', with: 1
+        select('Books', from: 'offers_category_id')
         fill_in 'offers_zipcode', with: '13854'
         fill_in 'offers_district', with: 'Pankow'
         fill_in 'offers_description', with: 'gebrauchtes Hochzeitskleid'
 
-        click_button 'Create Offer'
-
-        expect(page).to have_content('Offer was successfully created.')
-        end
+        click_button 'Create'
+        expect(page).to have_content('Hochzeitskleid')
+    end
 end
