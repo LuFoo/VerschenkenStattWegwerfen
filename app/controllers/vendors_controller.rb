df4d3dc96593dacdf4eaf5509fcedf48cd3a85c2
LuFoo <lucyk2406@gmail.com>
2018-01-03 22:23:56 +0100

    Fix for pagination of the search results for vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -10,14 +10,14 @@ class VendorsController < ApplicationController
     @page = (params[:page] || 0).to_i
     if params[:keywords].present?
         @keywords = params[:keywords]
-        customer_search_term = CustomerSearchTerm.new(@keywords)
-        @customers = Customer.where(
-            customer_search_term.where_clause,
-            customer_search_term.where_args).
-            order(customer_search_term.order).
+        vendor_search_term = VendorSearchTerm.new(@keywords)
+        @vendors = Vendor.where(
+            vendor_search_term.where_clause,
+            vendor_search_term.where_args).
+            order(vendor_search_term.order).
             offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
-        @customers = []
+        @vendors = []
     end
 
     @vendors = Vendor.all
